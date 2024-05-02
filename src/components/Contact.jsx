2ae026b79cb535c169a3c4d16a46d7ac5f15a8fc
Mihir Soni <ms6711534@gmail.com>
2024-05-02 20:22:34 +0530

    Toast updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,36 +8,48 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import React, { useRef } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
+import { SpinnerIcon } from "@chakra-ui/icons";
 
 const Contact = () => {
-    const form = useRef()
-    const toast = useToast()
+  const form = useRef();
+  const toast = useToast();
 
   const sendEmail = (e) => {
     e.preventDefault();
     e.target.reset();
     toast({
-      title: 'Sending Email...',
+      title: "Sending Email...",
       description: "Your message is being sending...",
-      variant: 'subtle',
-      status: 'warning',
+      variant: "subtle",
+      status: "info",
       duration: 3000,
       isClosable: false,
-    })
-    emailjs.sendForm('service_qd5z0sm', 'template_issrx1f', form.current, '0zDzKrX_j_Esh7sdc')
-      .then((result) => {
-        console.log(result.text);
-        toast({
-          title: 'Email sent!',
-          description: "Your message is sent to Mihir",
-          status: 'success',
-          duration: 3000,
-          isClosable: false,
-        })
-      }, (error) => {
-        console.log(error.text);
-      });
+      colorScheme: "blue",
+      icon: <SpinnerIcon />
+    });
+    emailjs
+      .sendForm(
+        "service_qd5z0sm",
+        "template_issrx1f",
+        form.current,
+        "0zDzKrX_j_Esh7sdc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast({
+            title: "Email sent!",
+            description: "Your message is sent to Mihir",
+            status: "success",
+            duration: 3000,
+            isClosable: false,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -65,7 +77,7 @@ const Contact = () => {
       </Box>
       <Flex
         flexDir={{ base: "column", lg: "row" }}
-        maxW={{base:"600px", lg:"fit-content"}}
+        maxW={{ base: "600px", lg: "fit-content" }}
         gap={"2rem"}
         m={"auto"}
         mt={"4rem"}
@@ -78,14 +90,27 @@ const Contact = () => {
         }}
         transition="box-shadow 0.5s ease-in-out"
       >
-        <Flex flexDir={{base:"column-reverse", lg:"column"}} justifyContent={"space-between"} gap={"2rem"}>
-          <Flex flexDir={"column"} gap={"1.5rem"} mt={{base:0, lg:"4rem"}}>
+        <Flex
+          flexDir={{ base: "column-reverse", lg: "column" }}
+          justifyContent={"space-between"}
+          gap={"2rem"}
+        >
+          <Flex flexDir={"column"} gap={"1.5rem"} mt={{ base: 0, lg: "4rem" }}>
             <Box
               _hover={{ border: "1px solid #689775" }}
               border={"1px solid transparent"}
               p={"0.6rem 1rem"}
               borderRadius={"0.4rem"}
-              onClick={()=>navigator.clipboard.writeText("+918340504980")}
+              onClick={() => {
+                navigator.clipboard.writeText("+918340504980");
+                toast({
+                  title: "Copied!",
+                  description: "Phone number is copied to clipboard.",
+                  status: "success",
+                  duration: 2000,
+                  isClosable: false,
+                });
+              }}
             >
               <Text as={"p"} color={"#AD8174"}>
                 <i className="bx bxs-phone"></i> +91-8340504980
@@ -96,7 +121,16 @@ const Contact = () => {
               border={"1px solid transparent"}
               p={"0.6rem 1rem"}
               borderRadius={"0.4rem"}
-              onClick={()=>navigator.clipboard.writeText("ms6711534@gmail.com")}
+              onClick={() =>
+                {navigator.clipboard.writeText("ms6711534@gmail.com")
+                toast({
+                  title: "Copied!",
+                  description: "Email is copied to clipboard.",
+                  status: "success",
+                  duration: 2000,
+                  isClosable: false,
+                });}
+              }
             >
               <Text as={"p"} color={"#AD8174"}>
                 <i className="bx bxl-gmail"></i> ms6711534@gmail.com
@@ -107,7 +141,12 @@ const Contact = () => {
               border={"1px solid transparent"}
               p={"0.6rem 1rem"}
               borderRadius={"0.4rem"}
-              onClick={()=>window.open("https://www.google.com/maps/place/Chapra,+Bihar/@25.7805724,84.6636231,12z/data=!3m1!4b1!4m6!3m5!1s0x3992bb17fdf6ea1f:0xe3bbfac01f4ab27a!8m2!3d25.7811397!4d84.7543413!16zL20vMDVwbnM4?entry=ttu", "_blank")}
+              onClick={() =>
+                window.open(
+                  "https://www.google.com/maps/place/Chapra,+Bihar/@25.7805724,84.6636231,12z/data=!3m1!4b1!4m6!3m5!1s0x3992bb17fdf6ea1f:0xe3bbfac01f4ab27a!8m2!3d25.7811397!4d84.7543413!16zL20vMDVwbnM4?entry=ttu",
+                  "_blank"
+                )
+              }
             >
               <Text as={"p"} color={"#AD8174"}>
                 <i className="bx bxs-map"></i> Chapra, India
@@ -120,7 +159,12 @@ const Contact = () => {
               borderRadius={"50%"}
               color={"#689775"}
               _hover={{ bg: "#689775", color: "rgb(0,0,6)" }}
-              onClick={()=>window.open("https://www.linkedin.com/in/mihir-13-soni", "_blank")}
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/mihir-13-soni",
+                  "_blank"
+                )
+              }
             >
               <Text as={"p"} fontSize={"1.6rem"}>
                 <i className="bx bxl-linkedin"></i>
@@ -131,7 +175,9 @@ const Contact = () => {
               borderRadius={"50%"}
               color={"#689775"}
               _hover={{ bg: "#689775", color: "rgb(0,0,6)" }}
-              onClick={()=>window.open("https://github.com/MrMihirSoni", "_blank")}
+              onClick={() =>
+                window.open("https://github.com/MrMihirSoni", "_blank")
+              }
             >
               <Text as={"p"} fontSize={"1.6rem"}>
                 <i className="bx bxl-github"></i>
@@ -142,7 +188,9 @@ const Contact = () => {
               borderRadius={"50%"}
               color={"#689775"}
               _hover={{ bg: "#689775", color: "rgb(0,0,6)" }}
-              onClick={()=>window.open("https://twitter.com/_mihir_soni","_blank")}
+              onClick={() =>
+                window.open("https://twitter.com/_mihir_soni", "_blank")
+              }
             >
               <Text as={"p"} fontSize={"1.6rem"}>
                 <i className="bx bxl-twitter"></i>
@@ -151,26 +199,46 @@ const Contact = () => {
           </Flex>
         </Flex>
         <Flex flexDir={"column"} gap={"1.5rem"}>
-          <form className='form' ref={form} onSubmit={sendEmail}>
+          <form className="form" ref={form} onSubmit={sendEmail}>
             <div>
               <label>Name :</label>
-              <div><i className='bx bx-user' ></i>
-              <input placeholder='Your Name' type="text" name="userName" required /></div>
+              <div>
+                <i className="bx bx-user"></i>
+                <input
+                  placeholder="Your Name"
+                  type="text"
+                  name="userName"
+                  required
+                />
+              </div>
             </div>
             <div>
               <label>Email :</label>
-              <div><i className="bx bxl-gmail"></i>
-              <input placeholder='Your Email' type="email" name="userEmail" required /></div>
+              <div>
+                <i className="bx bxl-gmail"></i>
+                <input
+                  placeholder="Your Email"
+                  type="email"
+                  name="userEmail"
+                  required
+                />
+              </div>
             </div>
             <div>
               <label>Message :</label>
               <div>
-              <textarea placeholder="Your Message" name="message" required /></div>
+                <textarea placeholder="Your Message" name="message" required />
+              </div>
             </div>
-            <input style={{
-              width: '100%',
-              cursor:'pointer'
-            }} className='submit' type="submit" value="Send" />
+            <input
+              style={{
+                width: "100%",
+                cursor: "pointer",
+              }}
+              className="submit"
+              type="submit"
+              value="Send"
+            />
           </form>
         </Flex>
       </Flex>
